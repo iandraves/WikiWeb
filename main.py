95e7a9f567306924cc2d76d51da2fdbd068b8e38
Ian Draves <iantdraves@gmail.com>
2021-07-08 17:39:57 +0000

    Input error handling

--- main.py
+++ main.py
@@ -3,20 +3,30 @@ import concurrent.futures
 
 def main():
 	# Searching for first page
-	first_page = input("First Page Title: ")
-	first_page_title = wikipedia.search(first_page, results=1, suggestion=False)[0]
-	print("Found: '" + first_page_title + "'")
+	try:
+		first_page = input("First Page Title: ")
+		first_page_title = wikipedia.search(first_page, results=1, suggestion=False)[0]
+		print("--> Found: '" + first_page_title + "'")
+	except:
+		print("Invalid search query! Try again.")
+		main()
 
 	# Searching for second page
-	second_page = input("Second Page Title: ")
-	second_page_title = wikipedia.search(second_page, results=1, suggestion=False)[0]
-	print("Found: '" + second_page_title + "'")
+	try:
+		second_page = input("Second Page Title: ")
+		second_page_title = wikipedia.search(second_page, results=1, suggestion=False)[0]
+		print("--> Found: '" + second_page_title + "'")
+	except:
+		print("Invalid search query! Try again.")
+		main()
 
 	# Loading pages
+	print("Loading page data...")
 	first_page_wikis = wikipedia.page(title=first_page_title, pageid=None, auto_suggest=False, redirect=True, preload=False).links
 	second_page_wikis = wikipedia.page(title=second_page_title, pageid=None, auto_suggest=False, redirect=True, preload=False).links
 
 	# Running concurrent searches
+	print("Crawling wikis...")
 	futures = []
 	with concurrent.futures.ThreadPoolExecutor() as executor:
 		first_search_path = executor.submit(search, first_page_title, second_page_title, first_page_wikis, 1, True)
@@ -28,9 +38,9 @@ def main():
 	# Waiting for match and outputting result
 	for future in concurrent.futures.as_completed(futures):
 		if future.result() == 1:
-			print("There is 1 degree of separation between '" + first_page + "' and " + "'" + second_page + "'")
+			print("There is 1 degree of separation between '" + first_page_title + "' and " + "'" + second_page_title + "'")
 		else:
-			print("There are " + str(future.result()) + " degrees of separation between '" + first_page + "' and " + "'" + second_page + "'")
+			print("There are " + str(future.result()) + " degrees of separation between '" + first_page_title + "' and " + "'" + second_page_title + "'")
 		break
 
 def search(start_page_title, end_page_title, wikis, degrees, primary_thread):
