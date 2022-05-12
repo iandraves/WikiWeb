7cf84837ce5961368244d7934f623c6c9b2dca6b
Ian Draves <iantdraves@gmail.com>
2022-05-12 15:53:10 -0600

    Updating README and changin license to txt file

--- README.md
+++ README.md
@@ -1,2 +1,89 @@
-# WikiWeb
-Calculates degrees of separation between any two given Wikipedia pages in real time without a pre-existing database
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/othneildrew/Best-README-Template">
+    <img src="img/web.png" alt="Logo" width="80" height="80">
+  </a>
+
+  <h3 align="center">WikiWeb</h3>
+
+  <p align="center">
+    A real-time degrees of separation calculator for Wikipedia pages :spider_web: 
+    <br />
+    <br />
+    <a href="https://replit.com/join/irmdocxcgz-iand">LIVE DEMO!</a>
+    ·
+    <a href="https://github.com/iandraves/WikiWeb/issues">Report Bug</a>
+    ·
+    <a href="https://github.com/iandraves/WikiWeb/issues">Request Feature</a>
+  </p>
+</div>
+
+<!-- ABOUT THE PROJECT -->
+
+## About The Project
+
+[![Product Name Screen Shot][product-screenshot]](https://example.com)
+
+I made WikiWeb because I thought it would be a good opportunity to practice my web crawling skills. Additionally, to my knowledge, there exist no other Wikipedia degrees of separation calculators that compute separation in real-time. This makes sense because without a relational database calculating degrees of separation between two very unrelated pages is practically uncomputable. That said, I think this is still a fun project because it leaves room to implement various optimizations - even if the time it takes for a search is unreasonable.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+<!-- SETUP -->
+
+## Setup
+
+1.  Clone the repo
+
+    HTTPS
+
+    ```sh
+    git clone https://github.com/iandraves/WikiWeb.git
+    ```
+
+    SSH
+
+    ```sh
+    git clone git@github.com:iandraves/WikiWeb.git
+    ```
+
+    GitHub CLI
+
+    ```sh
+    gh repo clone iandraves/WikiWeb
+    ```
+
+2.  Change to project directory
+    ```sh
+    cd WikiWeb
+    ```
+3.  Install dependencies
+    ```sh
+    pip install -r requirements.txt
+    ```
+4.  Run the program!
+    ```sh
+    python3 main.py
+    ```
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+<!-- BUILT WITH -->
+
+## Built With
+
+-   [Python](https://www.python.org/)
+-   [Wikipedia API](https://pypi.org/project/wikipedia/)
+
+<!-- LICENSE -->
+
+## License
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
+
+<p align="right">(<a href="#top">back to top</a>)</p>
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+
+[product-screenshot]: img/screenshot.png
--- main.py
+++ main.py
@@ -3,83 +3,96 @@ import concurrent.futures
 
 not_done = True
 
+
 def main():
-	# Searching for first page
-	try:
-		first_page = input("First Page Title: ")
-		first_page_title = wikipedia.search(first_page, results=1, suggestion=False)[0]
-		print("--> Found: '" + first_page_title + "'")
-	except:
-		print("Invalid search query! Try again.")
-		main()
+    # Searching for first page
+    try:
+        first_page = input("First Page Title: ")
+        first_page_title = wikipedia.search(
+            first_page, results=1, suggestion=False)[0]
+        print("--> Found: '" + first_page_title + "'")
+    except:
+        print("Invalid search query! Try again.")
+        main()
 
-	# Searching for second page
-	try:
-		second_page = input("Second Page Title: ")
-		second_page_title = wikipedia.search(second_page, results=1, suggestion=False)[0]
-		print("--> Found: '" + second_page_title + "'")
-	except:
-		print("Invalid search query! Try again.")
-		main()
+    # Searching for second page
+    try:
+        second_page = input("Second Page Title: ")
+        second_page_title = wikipedia.search(
+            second_page, results=1, suggestion=False)[0]
+        print("--> Found: '" + second_page_title + "'")
+    except:
+        print("Invalid search query! Try again.")
+        main()
 
-	# Loading pages
-	print("Loading page data...")
-	first_page_wikis = wikipedia.page(title=first_page_title, pageid=None, auto_suggest=False, redirect=True, preload=False).links
-	second_page_wikis = wikipedia.page(title=second_page_title, pageid=None, auto_suggest=False, redirect=True, preload=False).links
+    # Loading pages
+    print("Loading page data...")
+    first_page_wikis = wikipedia.page(
+        title=first_page_title, pageid=None, auto_suggest=False, redirect=True, preload=False).links
+    second_page_wikis = wikipedia.page(
+        title=second_page_title, pageid=None, auto_suggest=False, redirect=True, preload=False).links
 
-	# Running concurrent searches
-	print("Crawling wikis...")
-	futures = []
-	with concurrent.futures.ThreadPoolExecutor() as executor:
-		first_search_path = executor.submit(search, first_page_title, second_page_title, first_page_wikis, 1)
-		second_search_path = executor.submit(search, second_page_title, first_page_title, second_page_wikis, 1)
+    # Running concurrent searches
+    print("Crawling wikis...")
+    futures = []
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        first_search_path = executor.submit(
+            search, first_page_title, second_page_title, first_page_wikis, 1)
+        second_search_path = executor.submit(
+            search, second_page_title, first_page_title, second_page_wikis, 1)
 
-		futures.append(first_search_path)
-		futures.append(second_search_path)
+        futures.append(first_search_path)
+        futures.append(second_search_path)
 
-		# Waiting for match and outputting result
-		for future in concurrent.futures.as_completed(futures):
-			global not_done
-			not_done = False
-			if future.result() != None:
-				if future.result() == 1:
-					print("There is 1 degree of separation between '" + first_page_title + "' and " + "'" + second_page_title + "'")
-				else:
-					print("There are " + str(future.result()) + " degrees of separation between '" + first_page_title + "' and " + "'" + second_page_title + "'")
-		
+        # Waiting for match and outputting result
+        for future in concurrent.futures.as_completed(futures):
+            global not_done
+            not_done = False
+            if future.result() != None:
+                if future.result() == 1:
+                    print("There is 1 degree of separation between '" +
+                          first_page_title + "' and " + "'" + second_page_title + "'")
+                else:
+                    print("There are " + str(future.result()) + " degrees of separation between '" +
+                          first_page_title + "' and " + "'" + second_page_title + "'")
+
+
 def search(start_page_title, end_page_title, wikis, degrees):
-	while not_done:
-		# Checking current degree for match
-		if end_page_title in wikis:
-			return degrees
-		else:
-			degrees += 1
+    while not_done:
+        # Checking current degree for match
+        if end_page_title in wikis:
+            return degrees
+        else:
+            degrees += 1
 
-		# Checking next degree & creating subsequent one
-		new_wikis = []
-		for wiki_page in wikis:
-			if not not_done:
-				return None
-			else:
-				print("Checking '" + wiki_page + "' on degree level " + str(degrees), "(" + start_page_title + " --> " + end_page_title + ")")
+        # Checking next degree & creating subsequent one
+        new_wikis = []
+        for wiki_page in wikis:
+            if not not_done:
+                return None
+            else:
+                print("Checking '" + wiki_page + "' on degree level " + str(degrees),
+                      "(" + start_page_title + " --> " + end_page_title + ")")
 
-				try:
-					sub_wikis = wikipedia.page(title=wiki_page, pageid=None, auto_suggest=True, redirect=True, preload=False).links
-					
-					if end_page_title in sub_wikis:
-						return degrees
-					
-					new_wikis.extend(sub_wikis)
-				except:
-					continue
-		
-		# Removing duplicates
-		new_wikis = list(dict.fromkeys(new_wikis))
+                try:
+                    sub_wikis = wikipedia.page(
+                        title=wiki_page, pageid=None, auto_suggest=True, redirect=True, preload=False).links
 
-		# Searching new list
-		search(start_page_title, end_page_title, new_wikis, degrees)
-	
-	return None
+                    if end_page_title in sub_wikis:
+                        return degrees
+
+                    new_wikis.extend(sub_wikis)
+                except:
+                    continue
+
+        # Removing duplicates
+        new_wikis = list(dict.fromkeys(new_wikis))
+
+        # Searching new list
+        search(start_page_title, end_page_title, new_wikis, degrees)
+
+    return None
+
 
 if __name__ == '__main__':
-	main()
+    main()
